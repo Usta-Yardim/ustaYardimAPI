0a09672045ec168995a9915c9da60ca0078e385f
parzival-au20 <efsanevi_sefa@outlook.com>
2023-12-08 09:08:37 +0300

    UstaYardimApi first push and register login complited

---
+++ UstaYardımAPI/DTO/LoginDTO.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UstaYardımAPI.DTO
+{
+    public class LoginDTO
+    {
+        
+        [Required]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Password { get; set; } = null!;
+
+    }
+}
